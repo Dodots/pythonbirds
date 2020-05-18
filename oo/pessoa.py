935080bb8305afa64e9c81b962a629e921fe9ff5
Douglas <doouglascarvalho92@gmail.com>
2020-05-18 20:46:16 -0300

    Criados atributos de instância nome e idade

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,9 +1,17 @@
 class Pessoa:
+    def __init__(self, nome = None, idade = 28):
+        self.idade = idade
+        self.nome = nome
+
     def cumprimentar(self):
         return f'Olá {id(self)}'
 
 if __name__ == '__main__':
-    p = Pessoa()
+    p = Pessoa("Dodo")
     print(Pessoa.cumprimentar(p))
     print(id(p))
     print(p.cumprimentar())  #O jeito mais fácil de fazer.
+    print(p.nome)
+    p.nome = 'Douglas'
+    print(p.nome)
+    print(p.idade)
